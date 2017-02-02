8dacf18ff4e8e358cde00690f863b8b7994a510b
Aaron <aaron.cuddeback@gymratpacks.com>
2017-02-02 06:05:17 +0000

    make it better

--- src/config.js
+++ src/config.js
@@ -15,10 +15,10 @@
 // };
 
 module.exports = {
-  consumer_key: '',
-  consumer_secret: '',
-  access_token: '',
-  access_token_secret: '',
+  consumer_key: '3BLyfr5KekhD8tHsjNDd9ovAY',
+  consumer_secret: '4ZP1GioRBZ9TDJRXHkqj8uaivBy0yTaWCAkAq6VJ7ljMCMf82S',
+  access_token: '310897418-oqPWKblYrVSay76uqwi8BERq1g7ExYBlnHo4rMYe',
+  access_token_secret: 'C8gwe4tSuMPkLyPugtqQjof78GVew0coudWxmnFlQOBJf',
 };
 
 
--- src/helpers/strings.js
+++ src/helpers/strings.js
@@ -1,6 +1,8 @@
 module.exports = {
   queryString: [
-    ''
+    'freecodecamp',
+    '100daysofcode'
+    
   ],
   queryStringSubQuery: [
     ''

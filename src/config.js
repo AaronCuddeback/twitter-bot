49043f3218620213e54f0bc9d8f3ed70e58fe46a
Aaron <aaron.cuddeback@gymratpacks.com>
2017-02-02 06:14:08 +0000

    add environment variables

--- src/config.js
+++ src/config.js
@@ -7,14 +7,14 @@
 
 // Uncomment for when deploying to Heroku
 
-// module.exports = {
-//   consumer_key: process.env.CONSUMER_KEY,
-//   consumer_secret: process.env.CONSUMER_SECRET,
-//   access_token: process.env.ACCESS_TOKEN,
-//   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-// };
+ module.exports = {
+   consumer_key: process.env.CONSUMER_KEY,
+   consumer_secret: process.env.CONSUMER_SECRET,
+   access_token: process.env.ACCESS_TOKEN,
+   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+ };
 
-module.exports = {
+ module.exports = {
   consumer_key: '3BLyfr5KekhD8tHsjNDd9ovAY',
   consumer_secret: '4ZP1GioRBZ9TDJRXHkqj8uaivBy0yTaWCAkAq6VJ7ljMCMf82S',
   access_token: '310897418-oqPWKblYrVSay76uqwi8BERq1g7ExYBlnHo4rMYe',

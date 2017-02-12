2bb968aab30bdae74ce6c71fd330ca1f82f8a408
Aaron <aaron.cuddeback@gymratpacks.com>
2017-02-12 22:38:59 +0000

    updated responses

--- src/helpers/strings.js
+++ src/helpers/strings.js
@@ -19,13 +19,13 @@ module.exports = {
     'popular'
   ],
   responseString: [
-    'Hi @screenName thanks for the follow! !AC',
-    '@screenName thanks for following! !AC',
-    'Hey @screenName thanks for the follow! !AC',
-    'Thanks for following @screenName! !AC',
-    'Hey @screenName, thanks for following! !AC',
-    'Awesome @screenName, thanks for following! !AC',
-    'Thanks for the follow @screenName! !AC',
-    'Thanks for following @screenName! How are you today? !AC'
+    'Hi @screenName thanks for the follow! -AC',
+    '@screenName thanks for following! -AC',
+    'Hey @screenName thanks for the follow! -AC',
+    'Thanks for following @screenName! -AC',
+    'Hey @screenName, thanks for following! -AC',
+    'Awesome @screenName, thanks for following! -AC',
+    'Thanks for the follow @screenName! -AC',
+    'Thanks for following @screenName! How are you today? -AC'
   ]
 }
